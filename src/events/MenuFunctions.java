fb343620f069619b2656fff403134f56bedb49c9
OliverSM95 <olisimm2006@gmail.com>
2024-05-30 11:34:47 -0400

    Adding Events package: includes -Fishing -Gathering -Trading -Hunting - Menu Functions

--- src/events/MenuFunctions.java
+++ src/events/MenuFunctions.java
@@ -1,4 +1,18 @@
 package events;
 
+import javax.swing.*;
+
 public class MenuFunctions {
+
+
+
+    public static void landAcknowledgementMenu(){
+
+        JFrame frame = new JFrame();
+        frame.setSize(400,400);
+        JTextField acknowledgements = new JTextField("we acknowledge that we are on the traditional territory of many nations including the Mississaugas of the Credit, the Anishnabeg, the Chippewa, the Haudenosaunee and the Wendat peoples and is now home to many diverse First Nations, Inuit and Métis peoples.");
+// add audio file
+    }
+
+
 }
