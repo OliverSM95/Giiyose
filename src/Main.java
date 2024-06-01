0fa30b3f88e4f0d6e7ef1366359377c5e2d997bf
OliverSM95 <olisimm2006@gmail.com>
2024-06-01 00:21:29 -0400

    added land acknowledgements

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import events.MenuFunctions;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        MenuFunctions.landAcknowledgementMenu();
+
+
     }
 }
--- src/events/MenuFunctions.java
+++ src/events/MenuFunctions.java
@@ -1,6 +1,7 @@
 package events;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuFunctions {
 
@@ -10,8 +11,44 @@ public class MenuFunctions {
 
         JFrame frame = new JFrame();
         frame.setSize(400,400);
-        JTextField acknowledgements = new JTextField("we acknowledge that we are on the traditional territory of many nations including the Mississaugas of the Credit, the Anishnabeg, the Chippewa, the Haudenosaunee and the Wendat peoples and is now home to many diverse First Nations, Inuit and Métis peoples.");
-// add audio file
+
+        JPanel panel = new JPanel();
+        panel.setSize(300,400);
+        JTextArea acknowledgements = new JTextArea("we acknowledge that we are on the traditional territory"+" of many nations including the Mississaugas of the Credit,"+"the Anishnabeg, the Chippewa, the Haudenosaunee and the Wendat peoples"+" and is now home to many diverse First Nations, Inuit and Métis peoples.");
+acknowledgements.setSize(300,160);
+   ///     acknowledgements.setSize();
+        acknowledgements.setBackground(Color.orange);
+
+
+        acknowledgements.setLineWrap(true);// make words stay in panel
+        acknowledgements.setWrapStyleWord(true);
+        acknowledgements.setEditable(false); // Make it read-only if you don't want users to edit the text
+
+        JScrollPane scrollPane = new JScrollPane(acknowledgements);
+
+panel.add(scrollPane);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        frame.setVisible(true);
+
+
+
+
     }
 
 
