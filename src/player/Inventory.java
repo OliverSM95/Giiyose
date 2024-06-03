5701fda29cf63b0ed0a0a4b8442905e68285db38
efebolukbasi <bolukbasi.efe1@gmail.com>
2024-06-03 10:06:40 -0400

    Added packages Cleaned up

---
+++ src/player/Inventory.java
@@ -0,0 +1,19 @@
+package player;
+
+public class Inventory {
+    private int wood;
+    private int stone;
+    private int gold;
+    private int fur;
+    private int food;
+
+    public Inventory() {
+        this.wood = 0;
+        this.stone = 0;
+        this.gold = 0;
+        this.fur = 0;
+        this.food = 0;
+    }
+
+    // Getter and setter methods
+}
