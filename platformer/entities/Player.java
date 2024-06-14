2f1a193f8d46db7adc12ca44e4ad786b67e378ed
efebolukbasi <bolukbasi.efe1@gmail.com>
2024-06-14 11:02:38 -0400

    Added cannons

--- platformer/entities/Player.java
+++ platformer/entities/Player.java
@@ -47,6 +47,8 @@ public class Player extends Entity {
     private boolean attackChecked;
     private Playing playing;
 
+    private int tileY = 0;
+
     public Player(float x, float y, int width, int height, Playing playing) {
         super(x, y, width, height);
         this.playing = playing;
@@ -84,6 +86,7 @@ public class Player extends Entity {
         if (moving){
             checkPotionTouched();
             checkSpikesTouched();
+            tileY = (int) (hitbox.y / Game.TILES_SIZE);
         }
         if (attacking)
             checkAttack();
@@ -319,6 +322,10 @@ public class Player extends Entity {
 
         if (!IsEntityOnFloor(hitbox, lvlData))
             inAir = true;
+    }
+
+    public int getTileY(){
+        return tileY;
     }
 
 
